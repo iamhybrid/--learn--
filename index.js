4d1e43ac17f069257523e4745a7a4eb255ee1d78
iamhybrid <hello@iamhybrid.com>
2022-07-13 18:17:35 +0200

    1 file modified

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const settings = {
 
 const sketch = ({ context, width, height }) => {
 
-let x, y, w, h;
+let x, y, w, h, lW, fill, stroke;
 let angle, rx, ry;
 
-const num = 20;
+const num = 16;
 const degrees = -30;
 
 const rects = [];
@@ -23,37 +23,43 @@ for(let i = 0; i < num; i++){
   y = random.range(0, height);
   w = random.range(width * .2, width * .6);
   h = random.range(height * .1, height * .3);
-  lW = random.range(40) + 2;
-  rects.push({x, y, w, h, lW});
+
+  lW = random.range(h * .5) + 2;
+
+  fill = `rgba(255, 255, 255, 1)`;
+  stroke = `rgba(0, ${random.range(0,255)}, 255, 1)`;
+
+  rects.push({x, y, w, h, lW, fill, stroke});
 }
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
 
     rects.forEach(rect => {
 
-      const {x, y, w, h, lW} = rect;
+      const {x, y, w, h, lW, fill, stroke} = rect;
       context.save();
 
       context.translate(x,y);
-      context.strokeStyle = 'blue';
+      
 
-      drawSkewedRect({context, w, h, degrees, lW});
+      drawSkewedRect({context, w, h, degrees, lW, stroke, fill});
 
       context.restore();
     });
   };
 };
 
-const drawSkewedRect = ({context, w = 600, h = 200, degrees = -45, lW = 5}) => {
+const drawSkewedRect = ({context, w = 600, h = 200, degrees = -45, lW = 5, fill = 'white', stroke = 'black'}) => {
   const angle = math.degToRad(degrees);
   const rx = Math.cos(angle) * w;
   const ry = Math.sin(angle) * w;
 
   context.save();
   context.translate(rx * -.5, (ry + h) * -.5);
+
   context.lineWidth = lW;
+  context.strokeStyle = stroke;
+  context.fillStyle = fill;
 
   context.beginPath();
   context.moveTo(0, 0);
@@ -61,7 +67,9 @@ const drawSkewedRect = ({context, w = 600, h = 200, degrees = -45, lW = 5}) => {
   context.lineTo(rx, ry + h);
   context.lineTo(0, h);
   context.closePath();
+
   context.stroke();
+  //context.fill();
 
   context.restore();
 }
